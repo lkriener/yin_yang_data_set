b187a0596365fd2842eaa7b761a46abe19d88717
Clemens JS Schaefer <15638937+clee1994@users.noreply.github.com>
2021-01-06 12:51:50 -0500

    transforms

--- dataset.py
+++ dataset.py
@@ -1,7 +1,20 @@
 import numpy as np
 from torch.utils.data.dataset import Dataset
 
+class to_spike_train(object):
+    def __init__(self, T):
+        self.T = T
+        self.no_classes = 3
 
+    def __call__(self, sample):
+        no_neurons = sample[0].shape[0]
+        spike_train = np.zeros((self.T, no_neurons))
+        spike_train[[int(x * self.T) for x in sample[0]], np.arange(0,no_neurons)] = 1.
+
+        out_s =  np.zeros((self.T, self.no_classes))
+        out_s[:,sample[1]] = 1
+        return (spike_train, out_s)
+        
 class YinYangDataset(Dataset):
     def __init__(self, r_small=0.1, r_big=0.5, size=1000, seed=42, transform=None):
         super(YinYangDataset, self).__init__()
@@ -62,9 +75,9 @@ class YinYangDataset(Dataset):
         return np.sqrt((x - 0.5 * self.r_big)**2 + (y - self.r_big)**2)
 
     def __getitem__(self, index):
-
+        sample = (self.__vals[index], self.__cs[index])
         if self.transform:
-            sample = self.transform(self.__vals[index], self.__cs[index])
+            sample = self.transform(sample)
         return sample
 
     def __len__(self):
